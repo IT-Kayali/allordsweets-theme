b4e5951c720704664481d6483ba200b36cc46205
IT-Kayali <g.kayali@it-kayali.de>
2026-08-19 12:39:59 +0200

    feat: add child theme bootstrap

---
+++ woodmart-child/functions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Allord Sweets Child Theme bootstrap.
+ *
+ * @package AllordSweets
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+define( 'ALLORDSWEETS_THEME_VERSION', '0.1.0' );
+define( 'ALLORDSWEETS_THEME_DIR', get_stylesheet_directory() );
+define( 'ALLORDSWEETS_THEME_URI', get_stylesheet_directory_uri() );
+
+require_once ALLORDSWEETS_THEME_DIR . '/inc/setup.php';
+require_once ALLORDSWEETS_THEME_DIR . '/inc/assets.php';
