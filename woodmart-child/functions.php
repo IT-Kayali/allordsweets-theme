78547fcb770d01c6ccde1941d5be4ec4864f415e
IT-Kayali <g.kayali@it-kayali.de>
2026-08-19 19:17:00 +0200

    chore: bump header test version and load customizer

--- woodmart-child/functions.php
+++ woodmart-child/functions.php
@@ -7,10 +7,11 @@
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'ALLORDSWEETS_THEME_VERSION', '0.2.0' );
+define( 'ALLORDSWEETS_THEME_VERSION', '0.2.1' );
 define( 'ALLORDSWEETS_THEME_DIR', get_stylesheet_directory() );
 define( 'ALLORDSWEETS_THEME_URI', get_stylesheet_directory_uri() );
 
 require_once ALLORDSWEETS_THEME_DIR . '/inc/setup.php';
 require_once ALLORDSWEETS_THEME_DIR . '/inc/assets.php';
+require_once ALLORDSWEETS_THEME_DIR . '/inc/customizer.php';
 require_once ALLORDSWEETS_THEME_DIR . '/inc/header.php';
