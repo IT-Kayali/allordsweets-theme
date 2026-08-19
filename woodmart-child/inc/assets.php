4c5ed523b73ecaa422249a394543545d2e4c0092
IT-Kayali <g.kayali@it-kayali.de>
2026-08-19 13:03:44 +0200

    Phase 1: Child theme foundation
    Tested successfully on WordPress/WoodMart. Adds the modular Allord Sweets child-theme foundation and workflow documentation.

---
+++ woodmart-child/inc/assets.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Front-end assets.
+ *
+ * @package AllordSweets
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+function allordsweets_enqueue_assets() {
+	$version = ALLORDSWEETS_THEME_VERSION;
+
+	wp_enqueue_style(
+		'allordsweets-child',
+		get_stylesheet_uri(),
+		array( 'woodmart-style' ),
+		$version
+	);
+
+	$styles = array(
+		'global',
+		'header',
+		'footer',
+		'shop',
+		'product-card',
+		'single-product',
+		'responsive',
+	);
+
+	foreach ( $styles as $style ) {
+		wp_enqueue_style(
+			'allordsweets-' . $style,
+			ALLORDSWEETS_THEME_URI . '/assets/css/' . $style . '.css',
+			array( 'allordsweets-child' ),
+			$version
+		);
+	}
+
+	wp_enqueue_script(
+		'allordsweets-theme',
+		ALLORDSWEETS_THEME_URI . '/assets/js/theme.js',
+		array(),
+		$version,
+		true
+	);
+}
+add_action( 'wp_enqueue_scripts', 'allordsweets_enqueue_assets', 1000 );
